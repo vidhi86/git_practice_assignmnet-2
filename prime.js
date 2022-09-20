50a80c7c479cbbe6e9ee9ffcb7f7d316d47cb60d
Shivendra <shivendrapaliwal99990@gmail.com>
2022-09-20 22:29:28 +0530

    code_fixby_Shivendra

--- prime.js
+++ prime.js
@@ -1,14 +1,14 @@
-function Prime(num){
-    let count=0;
-    for (i=0 ; i<num ;i++){
-        if (num%i==0){
+function Prime(num) {
+    let count = 0;
+    for (i = 0; i < num; i++) {
+        if (num % i == 0) {
             count++;
         }
-    }if(count==2){
+    } if (count == 2) {
         return true;
     }
     return false;
 }
 
-let ans=Prime(63)
+let ans = Prime(63)
 console.log(ans)
